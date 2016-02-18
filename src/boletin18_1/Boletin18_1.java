ecedf127628a54fb18816128b107c7a9252740bd
Javii <Javii@Javi>
2016-02-18 08:53:15 +0100

    Commit inicial

---
+++ src/boletin18_1/Boletin18_1.java
@@ -0,0 +1,14 @@
+package boletin18_1;
+
+/**
+ *
+ * @author Javii
+ */
+public class Boletin18_1 {
+  public static void main(String[] args) {
+        ArrayDeNumeros obxArray= new ArrayDeNumeros();
+        obxArray.CargarArray();
+        obxArray.visualizarInverso();
+    }
+    
+}
